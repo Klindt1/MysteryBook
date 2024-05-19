c56d0ec3eb95f0163a80cfc481b5727f8b181a58
Klindt1 <160111295+Klindt1@users.noreply.github.com>
2024-05-19 22:15:24 +0200

    Create script.js
    My jave script

---
+++ script.js
@@ -0,0 +1,8 @@
+function buyMysteryBook() {
+    alert("Thank you for your purchase! Your mystery book will be delivered soon.");
+}
+
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you for contacting us! We will get back to you soon.");
+});
